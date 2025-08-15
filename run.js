4b3cf567bdde06daf6985bbeebad9669929c1d2b
Varnika <waytostudy18@gmail.com>
2025-08-15 17:17:23 +0530

    feat: Added multiplication functionality

--- run.js
+++ run.js
@@ -1,10 +1,10 @@
 let a = 5;
 let b = 10;
 
-let sum = a+b;
-let sub = a-b;
-
+let sum = a + b;
+let sub = a - b;
+let mul = a * b;
 
 console.log("Sum = ", sum);
 console.log("Sub = ", sub);
-
+console.log("mul =", mul);
